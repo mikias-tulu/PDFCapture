ce8c5c31581955d8f7934f7cbe73c3b83b20d90f
mikias-tulu <miki.tulu@gmail.com>
2025-04-20 21:05:00 +0300

    define shared components and utils

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,9 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white` }
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
